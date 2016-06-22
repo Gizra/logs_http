205133df5242bf7e8340f8981e93d22c943d996b
Or David <ordavidil@gmail.com>
2016-06-22 18:33:42 +0200

    fatal error progress.

--- src/Logger/LogsHttpLogger.php
+++ src/Logger/LogsHttpLogger.php
@@ -45,6 +45,16 @@ class LogsHttpLogger implements LoggerInterface {
   protected static $cache = [];
 
   /**
+   * WIP.
+   * On a fatal error Drupal is not creating an instance of this class.
+   */
+  public static function validateConfig() {
+    if (!static::$config) {
+      static::$config = \Drupal::service('config.factory')->get('logs_http.settings');
+    }
+  }
+
+  /**
    * Clear the events by setting a new array to the variable.
    */
   public static function reset() {
@@ -71,6 +81,7 @@ class LogsHttpLogger implements LoggerInterface {
    * {@inheritdoc}
    */
   public function log($level, $message, array $context = array()) {
+    static::validateConfig();
     if ($level > static::$config->get('severity_level')) {
       // Severity level is above the ones we want to log.
       return;
@@ -175,6 +186,7 @@ class LogsHttpLogger implements LoggerInterface {
    *  Returns TRUE if currently we should POST the data, otherwise returns FALSE.
    */
   public static function isEnabled() {
+    static::validateConfig();
     return !!static::$config->get('enabled') && !empty(static::getUrl());
   }
 
@@ -185,6 +197,7 @@ class LogsHttpLogger implements LoggerInterface {
    *  Returns the endpoint URL to POST data to.
    */
   public static function getUrl() {
+    static::validateConfig();
     return static::$config->get('url');
   }
 }
