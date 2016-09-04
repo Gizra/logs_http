a828af1c1877faea6405483c80e43c39d7796900
Amitaibu <amitai@gizra.com>
2016-09-04 13:19:00 +0300

    Add unit test

---
+++ tests/src/Unit/LogsHttpLoggerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\Tests\logs_http\Unit;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LogMessageParserInterface;
+use Drupal\Core\Logger\RfcLogLevel;
+use Drupal\logs_http\Logger\LogsHttpLogger;
+use Drupal\Tests\UnitTestCase;
+
+
+/**
+ * Tests the Logs Http logger service.
+ *
+ * @group logs_http
+ * @coversDefaultClass \Drupal\logs_http\Logger\LogsHttpLogger
+ */
+class LogsHttpLoggerTest extends UnitTestCase {
+
+  /**
+   * The config object.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $config;
+
+  /**
+   * The log message parser service.
+   *
+   * @var \Drupal\Core\Logger\LogMessageParserInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $logMessageParser;
+
+  /**
+   * The severity levels array.
+   *
+   * @var array
+   */
+  protected $severityLevels;
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->config = $this->prophesize(ConfigFactoryInterface::class);
+    $this->logMessageParser = $this->prophesize(LogMessageParserInterface::class);
+    $this->severityLevels = RfcLogLevel::getLevels();
+
+    $this
+      ->config
+      ->get('logs_http.settings')
+      ->willReturn($this->config->reveal());
+  }
+
+  /**
+   * Tests isEnabled method.
+   *
+   * @covers ::isEnabled
+   */
+  public function testIsEnabled() {
+    $this
+      ->config
+      ->get('enabled')
+      ->willReturn(TRUE);
+
+    $this
+      ->config
+      ->get('url')
+      ->willReturn($this->randomMachineName());
+
+
+    $logger = new LogsHttpLogger($this->config->reveal(), $this->logMessageParser->reveal());
+    $result = $logger->isEnabled();
+
+    $this->assertEquals(TRUE, $result);
+  }
+
+}
